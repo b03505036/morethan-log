2ed87f218ec323f4cabb1c29b86cda79b0643dbb
yiminou <ouyimin19@gmail.com>
2023-09-11 22:48:24 -0400

    update

--- site.config.js
+++ site.config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "YM Ou",
+    name: "Yimin Ou",
     image: "/profile.jpeg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "software developer",
-    bio: "do not go gentle \n into that good night.",
+    bio: "don't go gentle into that good night.",
     email: "ouyimin19@gmail.com",
     linkedin: "yi-min-ou-9b4533186/",
     github: "b03505036",
